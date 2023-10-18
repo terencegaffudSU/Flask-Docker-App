a21cd5757dcc56b463d1eaf93c555e5b14194ce0
terencegaffudSU <terence.gaffud@skillsunion.com>
2023-10-18 13:28:38 +0800

    Added circleci config

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return '<h1>Hello from Flask & Docker</h2>'
+    return '<h1>Hello from Flask & Docker</h1>'
 
+@app.route("/welcome")
+def welcome():
+    return "<h1>Welcome to the welcome page</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
